e0dac113d820f092324d4b2b67f7f019df9f1b49
buchammagarijahnavi04 <jahnavireddy904@gmail.com>
2026-02-06 12:14:25 +0530

    Adding Project Setup

---
+++ src/main/java/org/example/revhire/RevHireApplication.java
@@ -0,0 +1,13 @@
+package org.example.revhire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevHireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevHireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/revhire/RevHireApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.revhire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevHireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
