3a0039814f91b378c576a5cf0ed7cad985190f1b
asrita-pyda <asritasricharanipyda.1712@gmail.com>
2026-02-08 21:52:38 +0530

    8-creating-models

---
+++ src/main/java/org/example/revhire/model/ResumeSkills.java
@@ -0,0 +1,51 @@
+package org.example.revhire.model;
+import jakarta.persistence.*;
+import org.example.revhire.User;
+@Entity
+@Table(name = "resume_skills")
+public class ResumeSkills {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "skill_name", length = 100)
+    private String skillName;
+
+    public ResumeSkills() {
+    }
+
+    public ResumeSkills(Long id, User user, String skillName) {
+        this.id = id;
+        this.user = user;
+        this.skillName = skillName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getSkillName() {
+        return skillName;
+    }
+
+    public void setSkillName(String skillName) {
+        this.skillName = skillName;
+    }
+}
