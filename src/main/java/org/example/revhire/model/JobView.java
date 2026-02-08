780d707c809921632ede77c3014e4cade24ac504
tejamahindra <tejamahindramalleswarapu@gmail.com>
2026-02-08 22:27:53 +0530

    Added Job entities and job-related enums

---
+++ src/main/java/org/example/revhire/model/JobView.java
@@ -0,0 +1,58 @@
+package org.example.revhire.model;
+
+import jakarta.persistence.*;
+import org.apache.catalina.User;
+import org.example.revhire.enums.JobStatus;
+import org.example.revhire.enums.JobType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+@Entity
+@Table(name = "job_views")
+public class JobView {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "job_id")
+    private Job job;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDateTime viewedAt = LocalDateTime.now();
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getViewedAt() {
+        return viewedAt;
+    }
+}
