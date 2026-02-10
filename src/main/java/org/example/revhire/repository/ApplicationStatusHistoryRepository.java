5a657a1ec45f417a2ceae433c387d595968d7bfc
buchammagarijahnavi04 <jahnavireddy904@gmail.com>
2026-02-10 11:34:54 +0530

    Repositories has been added successfully for applications

---
+++ src/main/java/org/example/revhire/repository/ApplicationStatusHistoryRepository.java
@@ -0,0 +1,12 @@
+package org.example.revhire.repository;
+
+
+import org.example.revhire.model.ApplicationStatusHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ApplicationStatusHistoryRepository extends JpaRepository<ApplicationStatusHistory, Integer> {
+    List<ApplicationStatusHistory> findByApplication_Id(Integer applicationId);
+}
