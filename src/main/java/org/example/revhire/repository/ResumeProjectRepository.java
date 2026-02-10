844fdd97df14336de858c8f65e3e4570564ca144
asrita-pyda <asritasricharanipyda.1712@gmail.com>
2026-02-10 11:40:00 +0530

    Resume Repositories commited

---
+++ src/main/java/org/example/revhire/repository/ResumeProjectRepository.java
@@ -0,0 +1,12 @@
+package org.example.revhire.repository;
+
+import org.example.revhire.model.ResumeProjects;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ResumeProjectRepository extends JpaRepository<ResumeProjects, Integer> {
+    List<ResumeProjects> findByUser_Id(Integer userId);
+}
