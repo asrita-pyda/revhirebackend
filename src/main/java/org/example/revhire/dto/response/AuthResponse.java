21ea41de7b31b9dddad1f01b8661a449c870c11f
Chennamsetti-Harini <harinich966@gmail.com>
2026-02-20 00:29:12 +0530

    Added DTO request and response classes for user registration

---
+++ src/main/java/org/example/revhire/dto/response/AuthResponse.java
@@ -0,0 +1,90 @@
+package org.example.revhire.dto.response;
+
+import org.example.revhire.enums.Role;
+
+import java.util.Objects;
+
+public class AuthResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private Role role;
+    private String token; // For JWT implementation
+
+    public AuthResponse() {
+    }
+
+    public AuthResponse(Long id, String name, String email, Role role, String token) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.token = token;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        AuthResponse that = (AuthResponse) o;
+        return Objects.equals(id, that.id) && Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email);
+    }
+
+    @Override
+    public String toString() {
+        return "AuthResponse{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
+
